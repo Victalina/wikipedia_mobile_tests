62ed5caea9e93353466ed1443cf4c4888b04ee25
Victoria <viktalina@gmail.com>
2025-07-05 19:47:13 +0300

    fixed after review

--- src/test/java/tests/OpenArticleTests.java
+++ src/test/java/tests/OpenArticleTests.java
@@ -25,7 +25,7 @@ import static org.openqa.selenium.By.xpath;
 public class OpenArticleTests extends TestBase {
 
   @Test
-  @DisplayName("Проверка открытие статьи через поиск")
+  @DisplayName("Проверка открытия статьи через поиск")
   @Severity(SeverityLevel.CRITICAL)
   void openArticleTest() {
 
